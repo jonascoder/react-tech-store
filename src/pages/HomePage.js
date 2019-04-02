9717ce56a5dc9a826af02fcb635fa683c9b8dccb
Jonas Rodon <jonascoder@gmail.com>
2019-04-02 22:47:17 +0100

    Context Api Setup

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,9 +1,9 @@
 import React from "react";
-
+import { ProductConsumer } from "../context";
 export default function HomePage() {
   return (
     <>
-      <h1>Hello from HomePage</h1>
+      <ProductConsumer>{value => <h1>{value}</h1>}</ProductConsumer>
     </>
   );
 }
