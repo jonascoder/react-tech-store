2e677567e31c3d415e7dae98985a12a845bcd9f5
Jonas Rodon <jonascoder@gmail.com>
2019-04-02 19:34:36 +0100

    Make Structure

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 import React, { Component } from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+
+import Home from "./pages/HomePage";
+import About from "./pages/AboutPage";
+import Card from "./pages/CardPage";
+import Contact from "./pages/ContactPage";
+import Products from "./pages/ProductsPage";
+import SingleProduct from "./pages/SingleProductPage";
+import Default from "./pages/Default";
+
 class App extends Component {
   render() {
     return <h1>hello from the app</h1>;
