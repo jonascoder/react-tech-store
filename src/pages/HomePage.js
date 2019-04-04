b3ca777a490776a319d27399e0020e8a4f6a7683
Jonas Rodon <jonascoder@gmail.com>
2019-04-04 20:09:56 +0100

    Featured

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,6 +2,8 @@ import React from "react";
 import { ProductConsumer } from "../context";
 import Hero from "../components/Hero";
 import { Link } from "react-router-dom";
+import Services from "../components/HomePage/Services";
+import Featured from "../components/HomePage/Featured";
 export default function HomePage() {
   return (
     <>
@@ -10,6 +12,8 @@ export default function HomePage() {
           our products
         </Link>
       </Hero>
+      <Services />
+      <Featured />
     </>
   );
 }
